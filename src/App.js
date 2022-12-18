a50d7faf8a7d2aad0e38b014a1501d19c6b9127a
lucas <mrlucaszarate@gmail.com>
2022-12-18 19:37:55 -0300

    Reto Dia 1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <h1>Regalos:</h1>
+      <ul>
+        <li>Reloj</li>
+        <li>Tablet</li>
+        <li>Pelota</li>
+      </ul>
+    </div>
+      
+    </div>
+  );
+}
+
+export default App;
